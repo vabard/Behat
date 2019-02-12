9538271ea5371e6cd29f84e4b952b43ae1638c4d
vabard <33069762+vabard@users.noreply.github.com>
2019-02-12 14:39:52 +0100

    Update and rename JavascriptContext to JavascriptContext.php

--- JavascriptContext.php
+++ JavascriptContext.php
@@ -1,3 +1,13 @@
+  <?php
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use \Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Defines application features from the javscript context.
+ */
+class JavascriptContext extends RawDrupalContext implements SnippetAcceptingContext {
+  
   /**
    * Checks, that the maximum length of the CSS element is (?P<num>\d+).
    * Example: Then The ".title" element has a max length 10
@@ -19,3 +29,4 @@
       ));
     }
   }
+}
